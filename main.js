34323f55ac4c44f4aef592cf148e8a4b2e4b13e2
crowNiceToMeetYou <113548604+crowNiceToMeetYou@users.noreply.github.com>
2024-02-01 14:46:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let connectButton = document.getElementById('connect');
 let disconnectButton = document.getElementById('disconnect');
 let terminalContainer = document.getElementById('terminal');
-//let sendForm = document.getElementById('send-form');
-//let inputField = document.getElementById('input');
+let sendForm = document.getElementById('send-form');
+let inputField = document.getElementById('input');
 
 
 connectButton.addEventListener('click', function() {
